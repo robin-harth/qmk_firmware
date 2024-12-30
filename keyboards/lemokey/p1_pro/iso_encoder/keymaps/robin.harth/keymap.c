261c3da47e13023174a8076b2c8ee5d6a548f310
robin-harth <robin.harth.seligenstadt@gmail.com>
2024-12-30 01:37:03 +0100

    indicators

--- keyboards/lemokey/p1_pro/iso_encoder/keymaps/robin.harth/keymap.c
+++ keyboards/lemokey/p1_pro/iso_encoder/keymaps/robin.harth/keymap.c
@@ -75,3 +75,26 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     }
     return true;
 }
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (get_highest_layer(layer_state) > 0) {
+        uint8_t layer = get_highest_layer(layer_state);
+
+        for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+            for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+                uint8_t index = g_led_config.matrix_co[row][col];
+
+                if (index >= led_min && index < led_max && index != NO_LED &&
+                keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
+                    rgb_matrix_set_color(index, RGB_WHITE);
+                }
+            }
+        }
+    }
+    if (host_keyboard_led_state().caps_lock) {
+        for (uint8_t i = led_min; i <= 13; i++) {
+            rgb_matrix_set_color(i, RGB_GREEN);
+        }
+    }
+    return false;
+}
